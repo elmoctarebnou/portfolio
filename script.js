6d1d20242cc9931f06643b36553f1d50990bac06
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-10-26 10:37:17 -0500

    add skill

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const dropDownMenu = () => {
     const bottom = $('#bottom').toggleClass('rotateUp');
     const dropDownNav = $('#drop-nav-div').toggleClass('hide');
   });
-}
+  $("#drop-nav").on('click', 'li', (event) => { 
+    const current = event.currentTarget; 
+    const top = $('#top').toggleClass('rotateDown');
+    const middle = $('#middle').toggleClass('hide');
+    const bottom = $('#bottom').toggleClass('rotateUp');
+    const dropDownNav = $("#drop-nav-div").toggleClass('hide');
+  });
+};
 
 $(dropDownMenu);
