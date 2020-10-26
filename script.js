afd489f479ac6c79003cd29eed2491eb132502d2
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-10-26 01:01:54 -0500

    add drop down menu

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+const dropDownMenu = () => {
+  $(".nav-bar").on("click", '#hamburger-menu', (event) => {
+    event.preventDefault();
+    const top = $('#top').toggleClass('rotateDown');
+    const middle = $('#middle').toggleClass('hide');
+    const bottom = $('#bottom').toggleClass('rotateUp');
+    const dropDownNav = $('#drop-nav-div').toggleClass('hide');
+  });
+}
+
+$(dropDownMenu);
